0994f8384c57f34bd6205b61d881797e30c0795f
tmay02 <97641235+tmay02@users.noreply.github.com>
2022-02-20 16:17:02 -0800

    added all methods

--- MyDeque.java
+++ MyDeque.java
@@ -1,3 +1,39 @@
 public class MyDeque<E> implements DequeInterface<E> {
-    
+
+
+    public MyDeque(int initialCapacity){
+
+    }
+
+    public int size(){
+
+    }
+
+    public void expandCapacity(){
+
+    }
+
+    public void addFirst(E element){
+
+    }
+
+    public void addLast(E element){
+
+    }
+
+    public E removeFirst(){
+
+    }
+
+    public E removeLast(){
+
+    }
+
+    public E peekFirst(){
+
+    }
+
+    public E peekLast(){
+
+    }
 }

0969369c8bba4ca70d7df26d88b18ee97f11a4dd
reesewhitlock <rwhitlock@ucsd.edu>
2022-02-20 16:20:55 -0800

    constructor

--- MyDeque.java
+++ MyDeque.java
@@ -1,3 +1,17 @@
 public class MyDeque<E> implements DequeInterface<E> {
+    Object[] data;
+    int size;
+    int rear;
+    int front;
     
+    public MyDeque(int initialCapacity) {
+        if(initialCapacity < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.data = new Object[initialCapacity];
+        this.size = 0;
+        this.rear = 0;
+        this.front = 0;
+    }
 }

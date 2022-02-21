689c3faecbdedabb28eeae7e982381675a41d88e
reesewhitlock <rwhitlock@ucsd.edu>
2022-02-20 16:45:19 -0800

    remove and peek

--- MyDeque.java
+++ MyDeque.java
@@ -9,11 +9,11 @@ public class MyDeque<E> implements DequeInterface<E> {
             throw new IllegalArgumentException();
         }
 
-        data = new Object[initialCapacity];
+        this.data = new Object[initialCapacity];
         this.size = 0;
         this.rear = 0;
         this.front = 0;
-
+    }
 
     public int size(){
 
@@ -32,17 +32,54 @@ public class MyDeque<E> implements DequeInterface<E> {
     }
 
     public E removeFirst(){
+        if(this.size() == 0) {
+            return null;
+        }
 
+        E returnElement = (E) this.data[front];
+
+        this.data[front] = null;
+        this.size--;
+        if(this.front == this.size()) {
+            this.front = 0;
+        } else {
+            this.front++;
+        }
+
+        return returnElement;
     }
 
     public E removeLast(){
+        if(this.size() == 0) {
+            return null;
+        }
 
+        E returnElement = (E) this.data[rear];
+
+        this.data[rear] = null;
+        this.size--;
+        if(this.rear == 0) {
+            this.rear = this.size() - 1;
+        } else {
+            this.rear--;
+        }
+
+        return returnElement;
     }
 
     public E peekFirst(){
+        if(this.size() == 0) {
+            return null;
+        }
 
+        return (E) this.data[front];
     }
 
     public E peekLast(){
+        if(this.size() == 0) {
+            return null;
+        }
+
+        return (E) this.data[rear];
     }
 }
